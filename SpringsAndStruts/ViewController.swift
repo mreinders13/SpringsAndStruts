a6169c8a80a4864326afeef78965c70372e13b0e
Michael Reinders <Michael.H.Reinders@Gmail.com>
2019-07-30 19:10:17 -0700

    Initial Commit

---
+++ SpringsAndStruts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpringsAndStruts
+//
+//  Created by Michael Reinders on 7/30/19.
+//  Copyright © 2019 Michael Reinders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
